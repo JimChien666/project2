0591805491abaecb4e0ce6343afea60281229a60
簡佳恩 <jimchien@jianjiaendeMacBook-Pro.local>
2020-09-24 10:07:42 +0800

    first

---
+++ src/DAO/JdbcDao.java
@@ -0,0 +1,120 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public class JdbcDao {
+	private DataSource dataSource;
+	
+	
+	public DataSource getDataSource() {
+		if (this.dataSource == null) {
+			try {
+				InitialContext ctxt = new InitialContext();
+				DataSource dataSource = ( DataSource ) ctxt.lookup("java:comp/env/jdbc/xe");
+				
+				return dataSource;
+			} catch (NamingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return dataSource;
+	}
+	
+	public int insertAnimal(Animals animal) {
+		try (
+				Connection conn = getDataSource().getConnection();
+				Statement stmt = conn.createStatement();
+				PreparedStatement pstmt = conn.prepareStatement("INSERT INTO animals (id,age,animal_type,breed,coat,sex,img_url,is_alive) VALUES (?,?,?,?,?,?,?,?)");){
+		  	  int deptno = 1;
+			    String getAnimalIdSql = "SELECT animal_id.nextval FROM DUAL";
+		  	  
+		  	  //自取號機取得新部門的部門代號
+			    ResultSet rs = stmt.executeQuery(getAnimalIdSql);
+
+		      if (rs.next()) deptno = rs.getInt(1);
+
+		      rs.close();
+		      
+		      
+		      pstmt.setInt(1, animal.getId());
+		      pstmt.setString(2, animal.getAge());
+		      pstmt.setString(3, animal.getAnimalType());
+		      pstmt.setString(4, animal.getBreed());
+		      pstmt.setString(5, animal.getCoat());
+		      pstmt.setString(6, animal.getSex());
+		      pstmt.setString(7, animal.getImgUrl());
+		      pstmt.setBoolean(8, animal.isAlive());
+		      pstmt.executeUpdate();
+			  pstmt.clearParameters();
+		      
+		      stmt.close();
+		    return deptno;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public boolean updateAnimal(Animals animal) {
+		try(
+				Connection conn = getDataSource().getConnection();
+				PreparedStatement pstmt = conn.prepareStatement("update animals set age=?,animal_type=?,breed=?,coat=?,sex,img_url=?,is_alive=? where id=?");) {
+		  	  
+		      
+		      pstmt.setString(1, animal.getAge());
+		      pstmt.setString(2, animal.getAnimalType());
+		      pstmt.setString(3, animal.getBreed());
+		      pstmt.setString(4, animal.getCoat());
+		      pstmt.setString(5, animal.getSex());
+		      pstmt.setString(6, animal.getImgUrl());
+		      pstmt.setBoolean(7, animal.isAlive());
+		      pstmt.setInt(8, animal.getId());
+		      pstmt.executeUpdate();
+			  pstmt.clearParameters();
+		      
+		    return true;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public List<Animals> listAnimals() {
+		List<Animals> list = new ArrayList<>();
+		try (
+				Connection conn = getDataSource().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("select * from animals");){
+		  	  
+			while (rs.next()) {
+				Animals animal = new Animals();
+				animal.setId(rs.getInt("id"));
+				animal.setAge(rs.getString("age"));
+				animal.setAnimalType(rs.getString("animal_type"));
+				animal.setBreed(rs.getString("breed"));
+				animal.setCoat(rs.getString("coat"));
+				animal.setSex(rs.getString("sex"));
+				animal.setImgUrl(rs.getString("img_url"));
+				animal.setAlive(rs.getBoolean("is_alive"));
+				list.add(animal);
+			}
+			return list;
+		      
+		      
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+}
---
+++ src/TestDB.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAO.Animals;
+import DAO.JdbcDao;
+
+/**
+ * Servlet implementation class TestDB
+ */
+@WebServlet("/TestDB")
+public class TestDB extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestDB() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		JdbcDao jdbcDao = new JdbcDao();
+		request.setCharacterEncoding("UTF-8");
+		
+		System.out.println(jdbcDao.listAnimals());
+		List<Animals> list = jdbcDao.listAnimals();
+		for(Animals aml :list) {
+			System.out.println(aml.getAge());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
