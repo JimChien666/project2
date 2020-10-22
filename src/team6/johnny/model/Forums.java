e771809490f1c4b5c990574363a54bf8300b8b4a
x37658 <67813716+x37658@users.noreply.github.com>
2020-10-22 18:42:28 +0800

    article add into hibernate

---
+++ src/team6/johnny/model/Forums.java
@@ -0,0 +1,76 @@
+package tw.johnny.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "FORUMS")
+public class Forums {
+	private int id;
+	private int articleid;
+	private String content;
+	private Date createdat;
+	private int voteid;
+	private int memberid;
+
+	@Id @Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "ARTICLE_ID")
+	public int getArticleid() {
+		return articleid;
+	}
+
+	public void setArticleid(int articleid) {
+		this.articleid = articleid;
+	}
+
+	@Column(name = "CONTENT")
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Column(name = "CREATED_AT")
+	public Date getCreatedat() {
+		return createdat;
+	}
+
+	public void setCreatedat(Date createdat) {
+		this.createdat = createdat;
+	}
+
+	@Column(name = "VOTE_ID")
+	public int getVoteid() {
+		return voteid;
+	}
+
+	public void setVoteid(int voteid) {
+		this.voteid = voteid;
+	}
+
+	@Column(name = "MEMBER_ID")
+	public int getMemberid() {
+		return memberid;
+	}
+
+	public void setMemberid(int memberid) {
+		this.memberid = memberid;
+	}
+
+}
---
+++ src/team6/johnny/model/IArticleService.java
@@ -0,0 +1,13 @@
+package team6.johnny.model;
+
+import java.util.List;
+
+public interface IArticleService {
+
+	public Article insert(Article bean);
+	public Article select(int id);
+	public List<Article> selectAll();
+	public Article update(int id, String title, int activitysid, int articletypesid);
+	public boolean delete(int id);
+	
+}
