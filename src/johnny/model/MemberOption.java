3b26c1ec6c8db7ea62d4164b7b0e3db4c9c9b790
x37658 <67813716+x37658@users.noreply.github.com>
2020-10-23 19:59:10 +0800

    /* *  means need to edit or delete. */

---
+++ src/johnny/model/MemberOption.java
@@ -0,0 +1,45 @@
+package johnny.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MEMBER_OPTION")
+public class MemberOption {
+	private int id;
+	private int optionid;
+	private int memberid;
+
+	@Id @Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "OPTION_ID")
+	public int getOptionid() {
+		return optionid;
+	}
+
+	public void setOptionid(int optionid) {
+		this.optionid = optionid;
+	}
+
+	@Column(name = "MEMBER_ID")
+	public int getMemberid() {
+		return memberid;
+	}
+
+	public void setMemberid(int memberid) {
+		this.memberid = memberid;
+	}
+
+}
---
+++ src/johnny/model/Options.java
@@ -0,0 +1,45 @@
+package johnny.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "OPTIONS")
+public class Options {
+	private int id;
+	private int voteid;
+	private String content;
+
+	@Id @Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "VOTE_ID")
+	public int getVoteid() {
+		return voteid;
+	}
+
+	public void setVoteid(int voteid) {
+		this.voteid = voteid;
+	}
+
+	@Column(name = "CONTENT")
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
