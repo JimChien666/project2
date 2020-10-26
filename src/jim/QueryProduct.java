1b64015f4e8b57356b9e7b680e7fb05b65f3f506
JimCheng <jim810714@gmail.com>
2020-10-26 21:57:36 +0800

    10/26 21:57 查詢產品失敗

--- src/jim/QueryProduct.java
+++ src/jim/QueryProduct.java
@@ -2,23 +2,17 @@ package jim;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Blob;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import jim.entity.Products;
-import jim.entity.Products2;
+import jim.entity.ProductsDAO;
+import tw.reicheng.util.HibernateUtil;
 
-/**
- * Servlet implementation class InsertProduct
- */
 @WebServlet("/QueryProduct")
 public class QueryProduct extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -44,41 +38,58 @@ public class QueryProduct extends HttpServlet {
 		String search = "";
         PrintWriter out = response.getWriter();
         
-        if (request.getParameter("name") != null) {
-        	search = request.getParameter("name").trim();
-        }
-
-		String searchName = search.trim(); //把空白去掉!!
+//        if (request.getParameter("name") != null) {
+//        	search = request.getParameter("name").trim();
+//        }
+//
+//		String searchName = search.trim(); //把空白去掉!!
 //	    img = request.getParameter("img").trim(); //把空白去掉!!
 
-	    JdbcDao jdbcdao = new JdbcDao();
-
-    	List<Products> SearchList = new ArrayList<>();
-    	List<Products> list = jdbcdao.listProducts();
-    	for(Products product:list) {
-    		if(product.getName().contains(searchName)) {
-    			SearchList.add(product);
-    		}
-
-//			    out.println(list.getDescript());
-    	}
-      System.out.println("Get some SQL commands done!");
-      int size = SearchList.size();
-      if(size>0) {
-    	  
-    	  request.setAttribute("ProductList", SearchList);
-          RequestDispatcher rd= request.getRequestDispatcher("/jim/ProductList.jsp");
-          rd.forward(request, response);
-          return;
-      }
-      else {
-    	  System.out.println("dfjioasj");
-    	  request.setAttribute("noResult", "noResult");
-    	  
-    	  response.sendRedirect("/Project2/jim/QueryProduct.jsp");
+		
+		
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		ProductsDAO productsDao = new ProductsDAO(session);
+		Products products = productsDao.select(63);
+		
+		response.setContentType("text/html;charset=UTF-8");
+		
+		out.write("Id:" + products.getId() + "<br>");
+		out.write("Name:" + products.getName() + "<br>");
+		
+		out.close();
+		
+		
+//		
+//	    JdbcDao jdbcdao = new JdbcDao();
+//
+//    	List<Products> SearchList = new ArrayList<>();
+//    	List<Products> list = jdbcdao.listProducts();
+//    	for(Products product:list) {
+//    		if(product.getName().contains(searchName)) {
+//    			SearchList.add(product);
+//    		}
+//
+////			    out.println(list.getDescript());
+//    	}
+//      System.out.println("Get some SQL commands done!");
+//      int size = SearchList.size();
+//      if(size>0) {
+//    	  
+//    	  request.setAttribute("ProductList", SearchList);
+//          RequestDispatcher rd= request.getRequestDispatcher("/jim/ProductList.jsp");
+//          rd.forward(request, response);
+//          return;
+//      }
+//      else {
+//    	  System.out.println("dfjioasj");
+//    	  request.setAttribute("noResult", "noResult");
+//    	  
+//    	  response.sendRedirect("/Project2/jim/QueryProduct.jsp");
 //    	  RequestDispatcher rd= request.getRequestDispatcher("/jim/QueryProduct.jsp");
 //          rd.forward(request, response);
-      }
+//      }
 //          request.getSession(true).invalidate(); //關掉session
 //          request.getRequestDispatcher("jim/Thanks.jsp").include(request,response);
    
