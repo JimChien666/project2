6ae154fe047c0cd45231bbd506dd236384ed4e75
x37658 <67813716+x37658@users.noreply.github.com>
2020-10-27 10:15:38 +0800

    rename.

---
+++ src/johnny/controller/PostArticle.java
@@ -0,0 +1,138 @@
+package johnny.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import johnny.model.bean.Article;
+import johnny.model.dao.ArticleDAO;
+import johnny.util.HibernateUtil;
+
+/**
+ * Servlet implementation class DemoHibernateServletAction1
+ */
+@MultipartConfig
+@WebServlet("/PostArticle") //DemoHibernateServletAction2
+public class PostArticle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processAction(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processAction(request, response);
+	}
+
+	private void processAction(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		Session session = factory.openSession();
+		HttpSession hSession = request.getSession();
+		System.out.println("try");
+		try {
+			String title = "";
+			int activitysId = 0;
+			int articleType = 0;
+			int showArticle = 0;
+			int memberId = 0;
+//			int id = 0;
+			String activitysIdStr = "";
+			String articleTypeStr = "";
+			String showArticleStr = "";
+			String memberIdStr = "";
+//			String idStr = "";
+
+			Collection<Part> parts = request.getParts();
+			System.out.println("if");
+			if (parts != null) {
+				for (Part p : parts) {
+					String fldName = p.getName();
+					String value = request.getParameter(fldName);
+
+					if (p.getContentType() == null) {
+						if (fldName.equals("title")) {
+							title = value;
+							request.setAttribute("title", title);
+							
+//							idStr = value;
+//							idStr = idStr.trim();
+//							System.out.println(idStr);
+//							id = Integer.parseInt(idStr);
+//							request.setAttribute("id", id);
+//						} else if (fldName.equals("title")) {
+//							title = value;
+//							request.setAttribute("title", title);
+						} 	
+						else if (fldName.equals("activitysId")) {
+							activitysIdStr = value;
+							activitysIdStr = activitysIdStr.trim();
+							System.out.println(activitysIdStr);
+							activitysId = Integer.parseInt(activitysIdStr);
+							request.setAttribute("activitysId", activitysId);
+						} else if (fldName.equals("articleType")) {
+							articleTypeStr = value;
+							articleTypeStr = articleTypeStr.trim();
+							articleType = Integer.parseInt(articleTypeStr);
+							request.setAttribute("articleType", articleType);
+						} else if (fldName.equals("showArticle")) {
+							showArticleStr = value;
+							showArticleStr = showArticleStr.trim();
+							showArticle = Integer.parseInt(showArticleStr);
+							request.setAttribute("showArticle", showArticle);
+						} else if (fldName.equals("memberId")) {
+							memberIdStr = value;
+							memberIdStr = memberIdStr.trim();
+							memberId = Integer.parseInt(memberIdStr);
+							request.setAttribute("memberId", memberId);
+						}
+					} else {
+						System.out.println("錯誤，缺少必要的資料");
+					}
+
+				}
+			} else {
+				System.out.println("錯誤，缺少必要的資料");
+			}
+
+//			ArticleDao articleDao = new WriteArticleImpl_Jdbc();
+//
+//			ArticleBean ab = new ArticleBean(id, title, activitysId, articleType, showArticle, memberId);
+//
+//			articleDao.saveArticle(ab);
+
+			ArticleDAO aDAO = new ArticleDAO(session);
+
+			Article article = new Article(title, 1, articleType, 1, 1);
+
+			aDAO.insert(article);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/DemoHibernateServletAction1");
+			rd.forward(request, response);
+			return;
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
---
+++ src/johnny/controller/UpdateArticle.java
@@ -0,0 +1,95 @@
+package johnny.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import johnny.model.bean.Article;
+import johnny.model.dao.ArticleDAO;
+import johnny.util.HibernateUtil;
+
+/**
+ * Servlet implementation class DemoHibernateServletAction1
+ */
+@MultipartConfig
+@WebServlet("/UpdateArticle") //DemoHibernateServletAction3
+public class UpdateArticle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processAction(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processAction(request, response);
+	}
+
+	private void processAction(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		Session session = factory.openSession();
+		String aId = request.getParameter("id").trim();
+		
+		try {
+			String title = "";		
+			String idStr = "";
+			int id = 0;
+			int articleId = Integer.parseInt(aId);
+			Collection<Part> parts = request.getParts();
+			
+			if (parts != null) { 
+				for (Part p : parts) {
+					String fldName = p.getName();
+					String value = request.getParameter(fldName);
+					if (p.getContentType() == null) {   
+						if (fldName.equals("id")) {
+							idStr = value;
+							idStr = idStr.trim();
+							id = Integer.parseInt(idStr);								
+							request.setAttribute("id", id);					
+						}else if (fldName.equals("title")) {
+							title = value;
+							request.setAttribute("title", title);
+						}
+					}
+				}
+			}
+		
+			ArticleDAO aDAO = new ArticleDAO(session);
+
+//			Article article = new Article(id,title, 1, 1, 1, 1);
+			Article article = new Article();
+//			aDAO.update(article.getId(), article.getTitle(), 1, 1, 1, 1);
+//			aDAO.update(article);
+		
+		}catch (Exception e) {
+			e.printStackTrace(); 
+			RequestDispatcher rd = request.getRequestDispatcher("/DemoHibernateServletAction1");
+			rd.forward(request, response);
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
